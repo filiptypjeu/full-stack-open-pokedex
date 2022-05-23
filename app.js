576a86550aa18e50d7ce785b74008402d37cbfd0
Filip Stenbacka <filip.stenbacka@gmail.com>
2022-05-23 21:30:45 +0300

    Added health check.

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
+
+app.get('/health', (_, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_, res) => {
+  res.send('1')
+})
